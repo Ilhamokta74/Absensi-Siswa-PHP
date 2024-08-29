ce966866ef651b77304962f1fe5b967854bf4437
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-29 21:06:55 +0700

    Fix Fitur User

--- module/user/input_user.php
+++ module/user/input_user.php
@@ -1,144 +1,124 @@
 <?php
-if($_GET['act']=="input"){
-	?>
-          <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Input Data User</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Input Data User
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                    <form method="post" role="form" action="././module/simpan.php?act=input_user">
-
-                                <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Username</label>
-                                            <input class="form-control" placeholder="Username" name="nama">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Password</label>
-                                            <input class="form-control" placeholder="Password" name="pass">
-                                        </div>
-
-                                                                                <div class="form-group">
-                                            <label>Sekolah</label>
-                                            <select class="form-control" name="sekolah">
-  <?php
-	$sqla=mysql_query("select * from sekolah");
-	while($rsa=mysql_fetch_array($sqla)){
-
-	echo "<option value='$rsa[id]'>$rsa[nama]</option>";
-}
-
+if (isset($_GET['act']) && $_GET['act'] == "input") {
 ?>
-                                            </select>
-                                        </div>
-
-                                        <button type="submit" class="btn btn-success">Submit Button</button>
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Input Data User</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Input Data User
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <form method="post" role="form" action="./module/simpan.php?act=input_user">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label>Username</label>
+                                    <input class="form-control" placeholder="Username" name="nama" required>
                                 </div>
-                                <!-- /.col-lg-6 (nested) -->
-                                    </form>
-
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" placeholder="Password" name="pass" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Sekolah</label>
+                                    <select class="form-control" name="sekolah" required>
+                                        <?php
+                                        // Use MySQLi to fetch data
+                                        $result = $connection->query("SELECT * FROM sekolah");
+                                        while ($rsa = $result->fetch_assoc()) {
+                                            echo "<option value='{$rsa['id']}'>{$rsa['nama']}</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-success">Submit</button>
                             </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
+                            <!-- /.col-lg-6 (nested) -->
+                        </form>
                     </div>
-                    <!-- /.panel -->
+                    <!-- /.row (nested) -->
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.panel-body -->
             </div>
-            <!-- /.row -->
-           <?php } ?>
-
-
-
-           <?php
-if($_GET['act']=="edit_user"){
-	?>
-          <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Edit Data User</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Edit Data User
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
 <?php
-                            	$sql=mysql_query("select * from user where idu='$_GET[idu]'");
-								$rs=mysql_fetch_array($sql);
+}
+?>
+
+<?php
+if (isset($_GET['act']) && $_GET['act'] == "edit_user") {
+    // Fetch user data
+    $idu = intval($_GET['idu']);
+    $stmt = $connection->prepare("SELECT * FROM user WHERE idu = ?");
+    $stmt->bind_param('i', $idu);
+    $stmt->execute();
+    $user = $stmt->get_result()->fetch_assoc();
 
 ?>
-                                    <form method="post" role="form" action="././module/simpan.php?act=edit_user">
-<input type="hidden" name="idu" value="<?php echo $_GET['idu'] ?>" />
-
-                                <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Username</label>
-                                            <input class="form-control" placeholder="Username" name="nama" value="<?php echo "$rs[nama]"; ?>">
-</div>
-                                        <div class="form-group">
-
-                                            <label>Password</label>
-                                            <input class="form-control" placeholder="Password" name="pass">
-
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Sekolah</label>
-                                            <select class="form-control" name="sekolah">
-  <?php
-	$sqla=mysql_query("select * from sekolah");
-	while($rsa=mysql_fetch_array($sqla)){
-if($_SESSION['level']=="admin_guru"){
-if($rsa['id']==$_SESSION['id']){
-
-if($rs['id']==$rsa['id']){
-	echo "<option value='$rsa[id]' selected>$rsa[nama]</option>";
-}else{
-	echo "<option value='$rsa[id]'>$rsa[nama]</option>";
-}
-
-}
-}else{
-if($rs['id']==$rsa['id']){
-	echo "<option value='$rsa[id]' selected>$rsa[nama]</option>";
-}else{
-	echo "<option value='$rsa[id]'>$rsa[nama]</option>";
-}
-
-	}
-}?>
-                                          </select>
-                                        </div>
-
-                                        <button type="submit" class="btn btn-success">Submit Button</button>
-                                </div>
-                                <!-- /.col-lg-6 (nested) -->
-                                    </form>
-
-                            </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Edit Data User</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Edit Data User
                 </div>
-                <!-- /.col-lg-12 -->
+                <div class="panel-body">
+                    <div class="row">
+                        <form method="post" role="form" action="./module/simpan.php?act=edit_user">
+                            <input type="hidden" name="idu" value="<?php echo htmlspecialchars($user['idu']); ?>" />
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label>Username</label>
+                                    <input class="form-control" placeholder="Username" name="nama" value="<?php echo htmlspecialchars($user['nama']); ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" placeholder="Password" name="pass">
+                                    <small class="form-text text-muted">Leave blank if not changing.</small>
+                                </div>
+                                <div class="form-group">
+                                    <label>Sekolah</label>
+                                    <select class="form-control" name="sekolah" required>
+                                        <?php
+                                        $result_sekolah = $connection->query("SELECT * FROM sekolah");
+                                        while ($rsa = $result_sekolah->fetch_assoc()) {
+                                            $selected = ($rsa['id'] == $user['id']) ? 'selected' : '';
+                                            echo "<option value='{$rsa['id']}' $selected>{$rsa['nama']}</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-success">Submit</button>
+                            </div>
+                            <!-- /.col-lg-6 (nested) -->
+                        </form>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
             </div>
-            <!-- /.row -->
-            <?php } ?>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+<?php
+}
+?>
--- module/user/user.php
+++ module/user/user.php
@@ -1,66 +1,87 @@
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Data User</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Data User
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">User Name</th>
-                                            <th class="text-center">Level</th>
-                                            <th class="text-center">Sekolah</th>
-                                            <th class="text-center">Aksi</th>
-
-                                        </tr>
-                                    </thead>
-                                    <tbody>
 <?php
-$no=1;
-	if($_SESSION['level']=="admin_guru"){
-	$sql=mysql_query("select * from user where idu='$_SESSION[idu]'");
-	}else{
-	$sql=mysql_query("select * from user where idu<>'1'");
+include 'config/conn.php'; // Include your database connection
 
-	}
-	while($rs=mysql_fetch_array($sql)){
-	$sqla=mysql_query("select * from sekolah where id='$rs[id]'");
-	$rsa=mysql_fetch_array($sqla);
+$level = $_SESSION['level'];
+$idu = $_SESSION['idu'];
 
-
-?>                                        <tr class="odd gradeX">
-                                            <td><?php echo"$rs[nama]";  ?></td>
-                                            <td class="text-center"><?php echo"$rs[level]";  ?></td>
-                                            <td class="text-center"><?php echo"$rsa[nama]";  ?></td>
-
-                                        <td class="text-center"><a href="./././media.php?module=input_user&act=edit_user&idu=<?php echo $rs['idu'] ?>"><button type="button" class="btn btn-info">Edit</button>
- <?php 	if($_SESSION['level']!="admin_guru"){ ?>
- <a href="././module/simpan.php?act=hapus_user&idu=<?php echo $rs['idu'] ?>"><button type="button" class="btn btn-danger">Hapus</button></a></td>
-<?php } ?>
-
-                                        </tr>
-<?php
+// Prepare and execute the SQL query based on user level
+if ($level == "admin_guru") {
+    $stmt = $connection->prepare("SELECT * FROM user WHERE idu = ?");
+    $stmt->bind_param('i', $idu);
+} else {
+    $stmt = $connection->prepare("SELECT * FROM user WHERE idu <> 1");
 }
+
+$stmt->execute();
+$result = $stmt->get_result();
+
 ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Data User</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                Data User
             </div>
-            <!-- /.row -->
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th class="text-center">User Name</th>
+                                <th class="text-center">Level</th>
+                                <th class="text-center">Sekolah</th>
+                                <th class="text-center">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            while ($rs = $result->fetch_assoc()) {
+                                // Prepare and execute the query to get the school name
+                                $stmt_school = $connection->prepare("SELECT * FROM sekolah WHERE id = ?");
+                                $stmt_school->bind_param('i', $rs['id']);
+                                $stmt_school->execute();
+                                $result_school = $stmt_school->get_result();
+                                $rsa = $result_school->fetch_assoc();
+                            ?>
+                                <tr class="odd gradeX">
+                                    <td><?php echo htmlspecialchars($rs['nama']); ?></td>
+                                    <td class="text-center"><?php echo htmlspecialchars($rs['level']); ?></td>
+                                    <td class="text-center"><?php echo htmlspecialchars($rsa['nama']); ?></td>
+                                    <td class="text-center">
+                                        <a href="media.php?module=input_user&act=edit_user&idu=<?php echo htmlspecialchars($rs['idu']); ?>">
+                                            <button type="button" class="btn btn-info">Edit</button>
+                                        </a>
+                                        <?php if ($level != "admin_guru") { ?>
+                                            <a href="module/simpan.php?act=hapus_user&idu=<?php echo htmlspecialchars($rs['idu']); ?>">
+                                                <button type="button" class="btn btn-danger">Hapus</button>
+                                            </a>
+                                        <?php } ?>
+                                    </td>
+                                </tr>
+                            <?php
+                                $stmt_school->close();
+                            }
+                            $stmt->close();
+                            $connection->close();
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.table-responsive -->
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
