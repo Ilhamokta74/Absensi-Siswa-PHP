468cfcbb195f5d38b61bdf75708a20160972ca66
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-29 22:14:01 +0700

    Fix Fitur Absen

--- module/absen/absen.php
+++ module/absen/absen.php
@@ -1,18 +1,80 @@
 <?php
-$today= hari_ina(date("l"));
-$query=mysql_query("select * from hari where hari='$today'");
-$id_hari=mysql_fetch_array($query);
-$now= date("H:i:s");
-$aktifkan=mysql_query("update jadwal set aktif=1 where idh=$id_hari[idh] AND jam_mulai<= '$now' AND jam_selesai>= '$now'");
-$nonaktifkan=mysql_query("update jadwal set aktif=0 where idh<>$id_hari[idh]");
-$nonaktifkan2=mysql_query("update jadwal set aktif=0 where idh=$id_hari[idh] AND jam_mulai>= '$now' OR jam_selesai<= '$now'");
-$tentukan=mysql_query("select * from jadwal WHERE idj='$_GET[idj]'");
-$aktifgak=mysql_fetch_array($tentukan);
-if ($aktifgak['aktif']==1) {
-  include "input_absen.php";
+// Function to convert English day to Indonesian day
+function hari_ina($englishDay)
+{
+  $days = [
+    'Monday'    => 'Senin',
+    'Tuesday'   => 'Selasa',
+    'Wednesday' => 'Rabu',
+    'Thursday'  => 'Kamis',
+    'Friday'    => 'Jumat',
+    'Saturday'  => 'Sabtu',
+    'Sunday'    => 'Minggu'
+  ];
+  return $days[$englishDay] ?? '';
 }
-else {
-  echo "<center><br><h3>Maaf, Anda tidak bisa mengabsen siswa diluar jam pelajaran.</h3>
-    <a href=media.php?module=jadwal_mengajar><b>Kembali</b></a></center>";
+
+// Database connection
+$connection = new mysqli('localhost', 'username', 'password', 'database');
+
+// Check connection
+if ($connection->connect_error) {
+  die("Connection failed: " . $connection->connect_error);
 }
- ?>
+
+$today = hari_ina(date("l"));
+$now = date("H:i:s");
+
+// Prepare and execute queries
+$stmt = $connection->prepare("SELECT idh FROM hari WHERE hari = ?");
+$stmt->bind_param('s', $today);
+$stmt->execute();
+$id_hari = $stmt->get_result()->fetch_assoc();
+
+if ($id_hari) {
+  $id_hari = $id_hari['idh'];
+
+  // Update jadwal table based on current time
+  $stmt = $connection->prepare("
+        UPDATE jadwal 
+        SET aktif = CASE 
+            WHEN idh = ? AND jam_mulai <= ? AND jam_selesai >= ? THEN 1
+            ELSE 0
+        END
+    ");
+  $stmt->bind_param('iss', $id_hari, $now, $now);
+  $stmt->execute();
+
+  // Deactivate schedules that do not match the current day
+  $stmt = $connection->prepare("UPDATE jadwal SET aktif = 0 WHERE idh <> ?");
+  $stmt->bind_param('i', $id_hari);
+  $stmt->execute();
+
+  // Deactivate schedules that are out of the current time range
+  $stmt = $connection->prepare("
+        UPDATE jadwal 
+        SET aktif = 0 
+        WHERE idh = ? AND (jam_mulai >= ? OR jam_selesai <= ?)
+    ");
+  $stmt->bind_param('iss', $id_hari, $now, $now);
+  $stmt->execute();
+
+  // Check if the current schedule is active
+  $idj = $_GET['idj'];
+  $stmt = $connection->prepare("SELECT aktif FROM jadwal WHERE idj = ?");
+  $stmt->bind_param('i', $idj);
+  $stmt->execute();
+  $aktifgak = $stmt->get_result()->fetch_assoc();
+
+  if ($aktifgak['aktif'] == 1) {
+    include "input_absen.php";
+  } else {
+    echo "<center><br><h3>Maaf, Anda tidak bisa mengabsen siswa diluar jam pelajaran.</h3>
+        <a href='media.php?module=jadwal_mengajar'><b>Kembali</b></a></center>";
+  }
+} else {
+  echo "Error: Day not found.";
+}
+
+// Close connection
+$connection->close();
--- module/absen/input_absen.php
+++ module/absen/input_absen.php
@@ -1,231 +1,112 @@
-            <div class="row">
-                <div class="col-lg-12">
-					<h3 class="page-header"><strong>Input Data Absensi</h3>
-                </div>
-                <!-- /.col-lg-12 -->
+<?php
+$idj = $_GET['idj'];
+
+// Prepare and execute queries to fetch data
+$stmt = $connection->prepare("SELECT * FROM jadwal WHERE idj = ?");
+$stmt->bind_param('i', $idj);
+$stmt->execute();
+$arrayj = $stmt->get_result()->fetch_assoc();
+
+$stmt = $connection->prepare("SELECT * FROM mata_pelajaran WHERE idm = ?");
+$stmt->bind_param('i', $arrayj['idm']);
+$stmt->execute();
+$arraymp = $stmt->get_result()->fetch_assoc();
+
+$stmt = $connection->prepare("SELECT * FROM kelas WHERE idk = ?");
+$stmt->bind_param('i', $arrayj['idk']);
+$stmt->execute();
+$rsj = $stmt->get_result()->fetch_assoc();
+
+?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h3 class="page-header"><strong>Input Data Absensi</strong></h3>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <?php
+                echo "Data Siswa<br>";
+                echo "Kelas {$rsj['nama']} | {$arraymp['nama_mp']}";
+                ?>
             </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <?php $idj=$_GET['idj'];
-                            $dataquery=mysql_query("select * from jadwal where idj='$idj'");
-                            $arrayj=mysql_fetch_array($dataquery);
-                            $datamp=mysql_query("select * from mata_pelajaran where idm='$arrayj[idm]'");
-                            $arraymp=mysql_fetch_array($datamp);
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <form method="post" role="form" action="././module/simpan.php?act=input_absen&idj=<?php echo $idj ?>&tanggal=<?php echo date('Y-m-d') ?>&kelas=<?php echo $arrayj['idk'] ?>">
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">NIS</th>
+                                    <th class="text-center">Nama</th>
+                                    <th class="text-center">Keterangan</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $tg = date('Y-m-d');
+                                $stmt = $connection->prepare("SELECT * FROM siswa WHERE idk = ?");
+                                $stmt->bind_param('i', $arrayj['idk']);
+                                $stmt->execute();
+                                $result = $stmt->get_result();
 
-                            echo "Data Siswa";
-							$sqlj=mysql_query("select * from kelas where idk='$arrayj[idk]'");
-							$rsj=mysql_fetch_array($sqlj);
+                                while ($rs = $result->fetch_assoc()) {
+                                    $stmt = $connection->prepare("SELECT * FROM absen WHERE ids = ? AND tgl = ? AND idj = ?");
+                                    $stmt->bind_param('iss', $rs['ids'], $tg, $idj);
+                                    $stmt->execute();
+                                    $rsa = $stmt->get_result()->fetch_assoc();
+                                    $conk = $stmt->get_result()->num_rows;
 
-							echo "Kelas $rsj[nama] | $arraymp[nama_mp]";
-							 ?>
-                        </div>
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                           <form method="post" role="form" action="././module/simpan.php?act=input_absen&idj=<?php echo $idj ?>&tanggal=<?php echo date("Y-m-d") ?>&kelas=<?php echo $arrayj['idk'] ?>">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">NIS</th>
-                                            <th class="text-center">Nama</th>
-                                            <th class="text-center">Keterangan</th>
-
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-<?php
-$no=1;
-$tg=date("Y-m-d");
-$sqljk=mysql_query("select * from siswa where idk='$arrayj[idk]'");
-	while($rs=mysql_fetch_array($sqljk)){
-	$sqla=mysql_query("select * from absen where ids='$rs[ids]' and tgl='$tg' and idj='$idj'");
-	$rsa=mysql_fetch_array($sqla);
-	$conk=mysql_num_rows($sqla);
-	$sqlw=mysql_query("select * from kelas where idk='$rs[idk]'");
-	$rsw=mysql_fetch_array($sqlw);
-	$sqlb=mysql_query("select * from sekolah where id='$rsw[id]'");
-	$rsb=mysql_fetch_array($sqlb);
-
-?>                                        <tr class="odd gradeX">
-                                            <td><label style="font-weight:normal;"><?php echo "$rs[nis]";  ?></label></td>
-                                            <td><label style="font-weight:normal;"><?php echo "$rs[nama]";  ?></label></td>
-
-                                            <td class="text-center">
-                                                                                    <div class="form-group">
-
-<?php
-if($conk==0){
-?>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="A"  >A
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="I">I
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="S">S
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="M" >M
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="N" checked>N
-                                            </label>
-
-
-<?php } ?>
-
-<?php
-if($rsa['ket']=="A"){
-?>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="A" checked >A
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="I">I
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="S">S
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="M" >M
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="N" >N
-                                            </label>
-
-
-<?php } ?>
-<?php
-if($rsa['ket']=="I"){
-?>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="A"  >A
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="I" checked>I
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="S">S
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="M" >M
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="N" >N
-                                            </label>
-
-
-<?php } ?>
-<?php
-if($rsa['ket']=="S"){
-?>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="A"  >A
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="I" >I
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="S" checked>S
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="M" >M
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="N" >N
-                                            </label>
-
-
-<?php } ?>
-<?php
-if($rsa['ket']=="M"){
-?>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="A"  >A
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="I" >I
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="S" >S
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="M" checked>M
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="N" >N
-                                            </label>
-
-
-<?php } ?>
-<?php
-if($rsa['ket']=="N"){
-?>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="A"  >A
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="I" >I
-                                            </label>
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="S" >S
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="M" >M
-                                            </label>
-
-                                            <label class="radio-inline">
-                                                <input type="radio" name="<?php echo $rs['ids'] ?>" value="N" checked >N
-                                            </label>
-
-
-<?php } ?>
-
-
-                                        </div>
-
-                                            </td>
-
-                                        </tr>
-<?php
-}
-?>
-                                    </tbody>
-                                </table>
-                                        <button type="submit" class="btn btn-success">Simpan Data Absen</button>
-
-</form>
-                            </div>
-                            <!-- /.table-responsive -->
-<br>
-                            <div class="well">
-                                <h4>Keterangan Absensi</h4>
-                                <p>A = Tidak Masuk Tanpa Keterangan</p>
-                                <p>I = Tidak Masuk Ada Surat Ijin Atau Pemberitahuan</p>
-                                <p>S = Tidak Masuk Ada Surat Dokter Atau Pemberitahuan</p>
-                                <p>M = Hadir</p>
-                                <p>N = Belum di Absen</p>
-
-                            </div>
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
+                                ?>
+                                    <tr class="odd gradeX">
+                                        <td><label style="font-weight:normal;"><?php echo htmlspecialchars($rs['nis']); ?></label></td>
+                                        <td><label style="font-weight:normal;"><?php echo htmlspecialchars($rs['nama']); ?></label></td>
+                                        <td class="text-center">
+                                            <div class="form-group">
+                                                <?php
+                                                $statuses = ['A' => 'Tidak Masuk Tanpa Keterangan', 'I' => 'Tidak Masuk Ada Surat Ijin Atau Pemberitahuan', 'S' => 'Tidak Masuk Ada Surat Dokter Atau Pemberitahuan', 'M' => 'Hadir', 'N' => 'Belum di Absen'];
+                                                foreach ($statuses as $key => $value) {
+                                                    $checked = ($rsa['ket'] == $key) ? 'checked' : '';
+                                                    echo "<label class='radio-inline'>
+                                                        <input type='radio' name='{$rs['ids']}' value='$key' $checked>$key
+                                                    </label>";
+                                                }
+                                                ?>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                        <button type="submit" class="btn btn-success">Simpan Data Absen</button>
+                    </form>
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.table-responsive -->
+                <br>
+                <div class="well">
+                    <h4>Keterangan Absensi</h4>
+                    <?php
+                    foreach ($statuses as $key => $value) {
+                        echo "<p>$key = $value</p>";
+                    }
+                    ?>
+                </div>
             </div>
-            <!-- /.row -->
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+
+<?php
+// Close connection
+$connection->close();
+?>
