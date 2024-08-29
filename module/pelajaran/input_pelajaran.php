e47a65693c6611cef8bfecdaaec7852acb5201f2
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-29 21:45:14 +0700

    Fix Fitur Pelajaran

--- module/pelajaran/input_pelajaran.php
+++ module/pelajaran/input_pelajaran.php
@@ -1,96 +1,93 @@
 <?php
-if($_GET['act']=="input"){
-	?>
-          <div class="row">
-                <div class="col-lg-12">
-					<h3 class="page-header"><strong>Input Data Mata Pelajaran</strong></h3>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Input Data Mata Pelajaran
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                    <form method="post" role="form" action="././module/simpan.php?act=input_pelajaran">
-
-                                <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Mata Pelajaran</label>
-                                            <input type="text" class="form-control" placeholder="Mata Pelajaran" name="nama_mp">
-                                        </div>
-
-
-                                        <button type="submit" class="btn btn-success">Simpan</button>
-                                </div>
-                                <!-- /.col-lg-6 (nested) -->
-                                    </form>
-
-                            </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-           <?php } ?>
-
-         <?php
-if($_GET['act']=="edit_pelajaran"){
-	?>
-          <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Edit Data Mata Pelajaran</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Edit Data Mata Pelajaran
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-<?php
-                            	$sql=mysql_query("select * from mata_pelajaran where idm='$_GET[idm]'");
-								$rs=mysql_fetch_array($sql);
-
+if (isset($_GET['act']) && $_GET['act'] === "input") {
 ?>
-                                    <form method="post" role="form" action="././module/simpan.php?act=edit_pelajaran">
-<input type="hidden" name="idm" value="<?php echo $_GET['idm'] ?>" />
-
-                                <div class="col-lg-6">
-
-                                        <div class="form-group">
-
-                                            <label>Mata Pelajaran</label>
-                                            <input class="form-control" placeholder="Mata Pelajaran" name="nama_mp" value="<?php echo "$rs[nama_mp]"; ?>">
-
-                                        </div>
-
-
-                                        <button type="submit" class="btn btn-success">Simpan</button>
-                                </div>
-                                <!-- /.col-lg-6 (nested) -->
-                                    </form>
-
-                            </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h3 class="page-header"><strong>Input Data Mata Pelajaran</strong></h3>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Input Data Mata Pelajaran
                 </div>
-                <!-- /.col-lg-12 -->
+                <div class="panel-body">
+                    <div class="row">
+                        <form method="post" role="form" action="././module/simpan.php?act=input_pelajaran">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label>Mata Pelajaran</label>
+                                    <input type="text" class="form-control" placeholder="Mata Pelajaran" name="nama_mp" required>
+                                </div>
+                                <button type="submit" class="btn btn-success">Simpan</button>
+                            </div>
+                            <!-- /.col-lg-6 (nested) -->
+                        </form>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
             </div>
-            <!-- /.row -->
-            <?php } ?>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+<?php
+}
+?>
+
+<?php
+if (isset($_GET['act']) && $_GET['act'] === "edit_pelajaran") {
+    // Sanitize the `idm` parameter to prevent SQL Injection
+    $idm = intval($_GET['idm']);
+
+    // Prepare and execute the SQL query
+    $stmt = $connection->prepare("SELECT * FROM mata_pelajaran WHERE idm = ?");
+    $stmt->bind_param("i", $idm);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $rs = $result->fetch_assoc();
+?>
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Edit Data Mata Pelajaran</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Edit Data Mata Pelajaran
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <form method="post" role="form" action="././module/simpan.php?act=edit_pelajaran">
+                            <input type="hidden" name="idm" value="<?php echo htmlspecialchars($idm, ENT_QUOTES, 'UTF-8'); ?>" />
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label>Mata Pelajaran</label>
+                                    <input class="form-control" placeholder="Mata Pelajaran" name="nama_mp" value="<?php echo htmlspecialchars($rs['nama_mp'], ENT_QUOTES, 'UTF-8'); ?>" required>
+                                </div>
+                                <button type="submit" class="btn btn-success">Simpan</button>
+                            </div>
+                            <!-- /.col-lg-6 (nested) -->
+                        </form>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+<?php
+}
+?>
--- module/pelajaran/pelajaran.php
+++ module/pelajaran/pelajaran.php
@@ -1,53 +1,65 @@
-            <div class="row">
-                <div class="col-lg-12">
-					<h3 class="page-header"><strong>Data Mata Pelajaran</strong></h3>
-                </div>
-                <!-- /.col-lg-12 -->
+<div class="row">
+    <div class="col-lg-12">
+        <h3 class="page-header"><strong>Data Mata Pelajaran</strong></h3>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                Data Mata Pelajaran
             </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Data Mata Pelajaran
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">No</th>
-                                            <th class="text-center">Mata Pelajaran</th>
-                                            <th class="text-center">Aksi</th>
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th class="text-center">No</th>
+                                <th class="text-center">Mata Pelajaran</th>
+                                <th class="text-center">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            // Fetch data from the database
+                            $sql = "SELECT * FROM mata_pelajaran";
+                            $result = $connection->query($sql);
 
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-<?php
-$no=1;
-	$sql=mysql_query("select * from mata_pelajaran");
-	while($rs=mysql_fetch_array($sql)){
-
-?>                                        <tr class="odd gradeX">
-                                            <td><?php echo"$no";  ?></td>
-                                            <td ><?php echo"$rs[nama_mp]";  ?></td>
-
-                                        <td class="text-center"><a href="./././media.php?module=input_pelajaran&act=edit_pelajaran&idm=<?php echo $rs['idm'] ?>"><button type="button" class="btn btn-info">Edit</button></a>
-                                        <a href="././module/simpan.php?act=hapus_pelajaran&idm=<?php echo $rs['idm'] ?>"><button type="button" class="btn btn-danger">Hapus</button></a></td>
-                                        </tr>
-<?php
-$no++;}
-?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
+                            if ($result->num_rows > 0) {
+                                $no = 1;
+                                while ($rs = $result->fetch_assoc()) {
+                            ?>
+                                    <tr class="odd gradeX">
+                                        <td><?php echo htmlspecialchars($no, ENT_QUOTES, 'UTF-8'); ?></td>
+                                        <td><?php echo htmlspecialchars($rs['nama_mp'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                        <td class="text-center">
+                                            <a href="./././media.php?module=input_pelajaran&act=edit_pelajaran&idm=<?php echo urlencode($rs['idm']); ?>">
+                                                <button type="button" class="btn btn-info">Edit</button>
+                                            </a>
+                                            <a href="././module/simpan.php?act=hapus_pelajaran&idm=<?php echo urlencode($rs['idm']); ?>" onclick="return confirm('Are you sure you want to delete this item?');">
+                                                <button type="button" class="btn btn-danger">Hapus</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                            <?php
+                                    $no++;
+                                }
+                            } else {
+                                echo "<tr><td colspan='3' class='text-center'>No records found</td></tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.table-responsive -->
             </div>
-            <!-- /.row -->
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
