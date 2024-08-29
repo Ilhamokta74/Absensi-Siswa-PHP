f1b4851ae7f4235daae8c1271f636cd8b58658f7
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-29 21:48:49 +0700

    Fix Fitur Laporan

--- module/laporan/guru/rekap.php
+++ module/laporan/guru/rekap.php
@@ -1,116 +1,135 @@
 <?php
-$acuan=$_GET['idj'];
-$sqlacuan=mysql_query("select * from jadwal where idj='$acuan'");
-$rss=mysql_fetch_array($sqlacuan);
-$sqlkss=mysql_query("select * from kelas where idk='$rss[idk]'");
-$kss=mysql_fetch_array($sqlkss);
-$sqlmp=mysql_query("select * from mata_pelajaran where idm='$rss[idm]'");
-$nama_mp=mysql_fetch_array($sqlmp);
-$sqlidh=mysql_query("select * from hari where idh='$rss[idh]'");
-$nama_hari=mysql_fetch_array($sqlidh);
+// Sanitize input to prevent SQL Injection
+$idj = intval($_GET['idj']);
 
+// Prepare and execute queries
+$sqlacuan = $connection->prepare("SELECT * FROM jadwal WHERE idj = ?");
+$sqlacuan->bind_param("i", $idj);
+$sqlacuan->execute();
+$rss = $sqlacuan->get_result()->fetch_assoc();
+
+$sqlkss = $connection->prepare("SELECT * FROM kelas WHERE idk = ?");
+$sqlkss->bind_param("i", $rss['idk']);
+$sqlkss->execute();
+$kss = $sqlkss->get_result()->fetch_assoc();
+
+$sqlmp = $connection->prepare("SELECT * FROM mata_pelajaran WHERE idm = ?");
+$sqlmp->bind_param("i", $rss['idm']);
+$sqlmp->execute();
+$nama_mp = $sqlmp->get_result()->fetch_assoc();
+
+$sqlidh = $connection->prepare("SELECT * FROM hari WHERE idh = ?");
+$sqlidh->bind_param("i", $rss['idh']);
+$sqlidh->execute();
+$nama_hari = $sqlidh->get_result()->fetch_assoc();
+
+// Calculate the academic year
 $pecah = explode(" ", $tgl_lengkap);
-$satu=$pecah[0];
-$dua=$pecah[1];
-$tahun1=$pecah[2];
+$satu = $pecah[0];
+$dua = $pecah[1];
+$tahun1 = intval($pecah[2]);
 
-if ($dua=="Juli" || $dua=="Agustus" || $dua=="September" || $dua=="Oktober" || $dua=="November" || $dua=="Desember") {
-  $tahun2=$tahun1 + 1;
-}
-else {
-  $tahun2=$tahun1 - 1;
-}
+$tahun2 = ($dua == "Juli" || $dua == "Agustus" || $dua == "September" || $dua == "Oktober" || $dua == "November" || $dua == "Desember")
+  ? $tahun1 + 1
+  : $tahun1 - 1;
 ?>
-            <div class="row">
-                <div class="col-lg-12">
-                    <h3 class="page-header">
-                      <strong>
-                      <?php
-                      if ($dua=="Juli" || $dua=="Agustus" || $dua=="September" || $dua=="Oktober" || $dua=="November" || $dua=="Desember") {
-                        echo "Tahun Ajaran $tahun1 - $tahun2";
-                      }
-                      else {
-                        echo "Tahun Ajaran $tahun2 - $tahun1";
-                      }
-                      ?>
-                      </strong>
-                      <form action="module/laporan/guru/cetak_rekap.php" method="post">
-                        <input type="hidden" name="idj" value="<?php echo $acuan; ?>">
-                        <input type="hidden" name="tgl_lengkap" value="<?php echo $tgl_lengkap; ?>">
-                        <input style="float:right; margin-top:-40px;" class="btn btn-success btn-lg" type="submit" name="cetak" value="Cetak">
-                      </form>
-                    </h3>
 
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Data Absensi Kelas <?php echo "<b>$kss[nama] | $nama_mp[nama_mp]</b>";
-?>
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-<?php
-$sqlabsen=mysql_query("select DISTINCT tgl from absen where idj='$acuan'");
-$jumlahtanggal = mysql_num_rows($sqlabsen);
-$jumlahkolom=$jumlahtanggal+1;
- ?>
-<table class="table table-striped table-bordered table-hover">
-  <tr>
-    <td colspan="<?php echo $jumlahkolom; ?>" class="text-center info"><?php echo "<b>$nama_hari[hari], $rss[jam_mulai] - $rss[jam_selesai]</b>";?></td>
-  </tr>
-  <tr>
-    <td class="success text-center" rowspan="2"><b>Siswa</b></td>
-    <td colspan="<?php echo $jumlahtanggal; ?>" class="text-center success"><b>Tanggal (TGL/BLN)</b></td>
-  </tr>
-  <tr>
-    <?php
-    while ($tglnya=mysql_fetch_array($sqlabsen)) {
-      $pecah = explode("-", $tglnya['tgl']);
-      $satu=$pecah[0];
-      $dua=$pecah[1];
-      $tiga=$pecah[2];
-    ?>
-    <td class="text-center warning"><?php echo "<b>$tiga/$dua</b>"; ?></td>
-<?php } ?>
-  </tr>
-  <?php
-  $sqlrss=mysql_query("select * from siswa where idk='$rss[idk]'");
-  while ($siswanya=mysql_fetch_array($sqlrss)) {
-  ?>
-  <tr>
-    <td class="text-center warning"><?php echo $siswanya['nama']; ?></td>
-    <?php
-    $sqlabsen2=mysql_query("select ket from absen where ids='$siswanya[ids]' AND idj='$acuan'");
-    while ($ketnya=mysql_fetch_array($sqlabsen2)) {
-    ?>
-    <td class="text-center"><?php echo $ketnya['ket']; ?></td>
-    <?php } ?>
-  </tr>
-<?php } ?>
-</table>
+<div class="row">
+  <div class="col-lg-12">
+    <h3 class="page-header">
+      <strong>
+        <?php
+        echo ($dua == "Juli" || $dua == "Agustus" || $dua == "September" || $dua == "Oktober" || $dua == "November" || $dua == "Desember")
+          ? "Tahun Ajaran $tahun1 - $tahun2"
+          : "Tahun Ajaran $tahun2 - $tahun1";
+        ?>
+      </strong>
+      <form action="module/laporan/guru/cetak_rekap.php" method="post">
+        <input type="hidden" name="idj" value="<?php echo htmlspecialchars($acuan); ?>">
+        <input type="hidden" name="tgl_lengkap" value="<?php echo htmlspecialchars($tgl_lengkap); ?>">
+        <input style="float:right; margin-top:-40px;" class="btn btn-success btn-lg" type="submit" name="cetak" value="Cetak">
+      </form>
+    </h3>
+  </div>
+  <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+  <div class="col-lg-12">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        Data Absensi Kelas <?php echo "<b>$kss[nama] | $nama_mp[nama_mp]</b>"; ?>
+      </div>
+      <!-- /.panel-heading -->
+      <div class="panel-body">
+        <div class="table-responsive">
+          <?php
+          // Fetch distinct dates of attendance
+          $sqlabsen = $connection->prepare("SELECT DISTINCT tgl FROM absen WHERE idj = ?");
+          $sqlabsen->bind_param("i", $idj);
+          $sqlabsen->execute();
+          $absen_dates = $sqlabsen->get_result();
 
-                            </div>
+          $jumlahtanggal = $absen_dates->num_rows;
+          $jumlahkolom = $jumlahtanggal + 1;
+          ?>
+          <table class="table table-striped table-bordered table-hover">
+            <tr>
+              <td colspan="<?php echo $jumlahkolom; ?>" class="text-center info">
+                <?php echo "<b>$nama_hari[hari], $rss[jam_mulai] - $rss[jam_selesai]</b>"; ?>
+              </td>
+            </tr>
+            <tr>
+              <td class="success text-center" rowspan="2"><b>Siswa</b></td>
+              <td colspan="<?php echo $jumlahtanggal; ?>" class="text-center success"><b>Tanggal (TGL/BLN)</b></td>
+            </tr>
+            <tr>
+              <?php
+              while ($tglnya = $absen_dates->fetch_assoc()) {
+                $pecah = explode("-", $tglnya['tgl']);
+                $satu = $pecah[0];
+                $dua = $pecah[1];
+                $tiga = $pecah[2];
+              ?>
+                <td class="text-center warning"><?php echo "<b>$tiga/$dua</b>"; ?></td>
+              <?php } ?>
+            </tr>
+            <?php
+            $sqlrss = $connection->prepare("SELECT * FROM siswa WHERE idk = ?");
+            $sqlrss->bind_param("i", $rss['idk']);
+            $sqlrss->execute();
+            $siswa_result = $sqlrss->get_result();
 
-
-                            <!-- /.table-responsive -->
-                            <div class="well">
-                                <h4>Keterangan Absensi</h4>
-                                <p>A = Tidak Masuk Tanpa Keterangan<br>
-                                <p>I = Tidak Masuk Ada Surat Ijin Atau Pemberitahuan</p>
-                                <p>S = Tidak Masuk Ada Surat Dokter Atau Pemberitahuan</p>
-                                <p>M = Hadir</p>
-                            </div>
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
+            while ($siswanya = $siswa_result->fetch_assoc()) {
+            ?>
+              <tr>
+                <td class="text-center warning"><?php echo htmlspecialchars($siswanya['nama']); ?></td>
+                <?php
+                $sqlabsen2 = $connection->prepare("SELECT ket FROM absen WHERE ids = ? AND idj = ?");
+                $sqlabsen2->bind_param("ii", $siswanya['ids'], $idj);
+                $sqlabsen2->execute();
+                $absen_notes = $sqlabsen2->get_result();
+                while ($ketnya = $absen_notes->fetch_assoc()) {
+                ?>
+                  <td class="text-center"><?php echo htmlspecialchars($ketnya['ket']); ?></td>
+                <?php } ?>
+              </tr>
+            <?php } ?>
+          </table>
+        </div>
+        <!-- /.table-responsive -->
+        <div class="well">
+          <h4>Keterangan Absensi</h4>
+          <p>A = Tidak Masuk Tanpa Keterangan<br>
+          <p>I = Tidak Masuk Ada Surat Ijin Atau Pemberitahuan</p>
+          <p>S = Tidak Masuk Ada Surat Dokter Atau Pemberitahuan</p>
+          <p>M = Hadir</p>
+        </div>
+      </div>
+      <!-- /.panel-body -->
+    </div>
+    <!-- /.panel -->
+  </div>
+  <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
--- module/laporan/siswa/rekap.php
+++ module/laporan/siswa/rekap.php
@@ -1,105 +1,120 @@
 <?php
-$acuan=$_GET['idj'];
-$sqlacuan=mysql_query("select * from jadwal where idj='$acuan'");
-$rss=mysql_fetch_array($sqlacuan);
-$sqlrss=mysql_query("select * from siswa where nis='$uidi'");
-$siswa=mysql_fetch_array($sqlrss);
-$sqlmp=mysql_query("select * from mata_pelajaran where idm='$rss[idm]'");
-$nama_mp=mysql_fetch_array($sqlmp);
-$sqlidh=mysql_query("select * from hari where idh='$rss[idh]'");
-$nama_hari=mysql_fetch_array($sqlidh);
+// Sanitize input to prevent SQL Injection
+$idj = intval($_GET['idj']);
 
+// Fetch data from `jadwal`
+$sqlacuan = $connection->prepare("SELECT * FROM jadwal WHERE idj = ?");
+$sqlacuan->bind_param("i", $idj);
+$sqlacuan->execute();
+$rss = $sqlacuan->get_result()->fetch_assoc();
+
+// Fetch data from `siswa`
+$sqlrss = $connection->prepare("SELECT * FROM siswa WHERE nis = ?");
+$sqlrss->bind_param("s", $uidi);
+$sqlrss->execute();
+$siswa = $sqlrss->get_result()->fetch_assoc();
+
+// Fetch data from `mata_pelajaran`
+$sqlmp = $connection->prepare("SELECT * FROM mata_pelajaran WHERE idm = ?");
+$sqlmp->bind_param("i", $rss['idm']);
+$sqlmp->execute();
+$nama_mp = $sqlmp->get_result()->fetch_assoc();
+
+// Fetch data from `hari`
+$sqlidh = $connection->prepare("SELECT * FROM hari WHERE idh = ?");
+$sqlidh->bind_param("i", $rss['idh']);
+$sqlidh->execute();
+$nama_hari = $sqlidh->get_result()->fetch_assoc();
+
+// Calculate the academic year
 $pecah = explode(" ", $tgl_lengkap);
-$satu=$pecah[0];
-$dua=$pecah[1];
-$tahun1=$pecah[2];
+$satu = $pecah[0];
+$dua = $pecah[1];
+$tahun1 = intval($pecah[2]);
 
-if ($dua=="Juli" || $dua=="Agustus" || $dua=="September" || $dua=="Oktober" || $dua=="November" || $dua=="Desember") {
-  $tahun2=$tahun1 + 1;
-}
-else {
-  $tahun2=$tahun1 - 1;
-}
+$tahun2 = ($dua == "Juli" || $dua == "Agustus" || $dua == "September" || $dua == "Oktober" || $dua == "November" || $dua == "Desember") ? $tahun1 + 1 : $tahun1 - 1;
 ?>
-            <div class="row">
-                <div class="col-lg-12">
-                    <h3 class="page-header">
-                      <strong>
-                      <?php
-                      if ($dua=="Juli" || $dua=="Agustus" || $dua=="September" || $dua=="Oktober" || $dua=="November" || $dua=="Desember") {
-                        echo "Tahun Ajaran $tahun1 - $tahun2 ($nama_mp[nama_mp])";
-                      }
-                      else {
-                        echo "Tahun Ajaran $tahun2 - $tahun1 ($nama_mp[nama_mp])";
-                      }
-                      ?>
-                      </strong>
-                    </h3>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Data Absensi Siswa
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-<?php
-$sqlabsen=mysql_query("select DISTINCT tgl from absen where idj='$acuan' AND ids='$siswa[ids]'");
- ?>
-<table class="table table-striped table-bordered table-hover">
-  <tr>
-    <td colspan="2" class="text-center info"><?php echo "<b>$nama_hari[hari], $rss[jam_mulai] - $rss[jam_selesai]</b>";?></td>
-  </tr>
-  <tr>
-    <td class="success text-center" rowspan="2"><b>Tanggal (TGL/BLN)</b></td>
-    <td class="text-center success"><b>Siswa</b></td>
-  </tr>
-  <tr>
-    <?php
-    $siswanya=mysql_fetch_array($sqlrss);
-    ?>
-    <td class="text-center warning"><?php echo "<b>$usre | Kelas : $rs[nama]</b>"; ?></td>
-  </tr>
-  <?php
-  while ($tglnya=mysql_fetch_array($sqlabsen)) {
-    $pecah = explode("-", $tglnya['tgl']);
-    $satu=$pecah[0];
-    $dua=$pecah[1];
-    $tiga=$pecah[2];
-  ?>
-  <tr>
-    <td class="text-center warning"><?php echo "<b>$tiga/$dua</b>"; ?></td>
-    <?php
-    $sqlabsen2=mysql_query("select ket from absen where ids='$siswa[ids]' AND idj='$acuan' AND tgl='$tglnya[tgl]'");
-    while($ketnya=mysql_fetch_array($sqlabsen2)) {
-    ?>
-    <td class="text-center"><?php echo $ketnya['ket']; ?></td>
-    <?php } ?>
-  </tr>
-<?php } ?>
-</table>
 
-                            </div>
-
-
-                            <!-- /.table-responsive -->
-                            <div class="well">
-                                <h4>Keterangan Absensi</h4>
-                                <p>A = Tidak Masuk Tanpa Keterangan</p>
-                                <p>I = Tidak Masuk Ada Surat Ijin Atau Pemberitahuan</p>
-                                <p>S = Tidak Masuk Ada Surat Dokter Atau Pemberitahuan</p>
-                                <p>M = Hadir</p>
-                            </div>
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
+<div class="row">
+  <div class="col-lg-12">
+    <h3 class="page-header">
+      <strong>
+        <?php
+        echo ($dua == "Juli" || $dua == "Agustus" || $dua == "September" || $dua == "Oktober" || $dua == "November" || $dua == "Desember")
+          ? "Tahun Ajaran $tahun1 - $tahun2 ($nama_mp[nama_mp])"
+          : "Tahun Ajaran $tahun2 - $tahun1 ($nama_mp[nama_mp])";
+        ?>
+      </strong>
+    </h3>
+  </div>
+  <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+  <div class="col-lg-12">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        Data Absensi Siswa
+      </div>
+      <!-- /.panel-heading -->
+      <div class="panel-body">
+        <div class="table-responsive">
+          <?php
+          // Fetch distinct dates of attendance
+          $sqlabsen = $connection->prepare("SELECT DISTINCT tgl FROM absen WHERE idj = ? AND ids = ?");
+          $sqlabsen->bind_param("is", $idj, $siswa['ids']);
+          $sqlabsen->execute();
+          $absen_dates = $sqlabsen->get_result();
+          ?>
+          <table class="table table-striped table-bordered table-hover">
+            <tr>
+              <td colspan="2" class="text-center info">
+                <?php echo "<b>$nama_hari[hari], $rss[jam_mulai] - $rss[jam_selesai]</b>"; ?>
+              </td>
+            </tr>
+            <tr>
+              <td class="success text-center" rowspan="2"><b>Tanggal (TGL/BLN)</b></td>
+              <td class="text-center success"><b>Siswa</b></td>
+            </tr>
+            <tr>
+              <td class="text-center warning"><?php echo "<b>$usre | Kelas : $rs[nama]</b>"; ?></td>
+            </tr>
+            <?php
+            while ($tglnya = $absen_dates->fetch_assoc()) {
+              $pecah = explode("-", $tglnya['tgl']);
+              $satu = $pecah[0];
+              $dua = $pecah[1];
+              $tiga = $pecah[2];
+            ?>
+              <tr>
+                <td class="text-center warning"><?php echo "<b>$tiga/$dua</b>"; ?></td>
+                <?php
+                // Fetch attendance notes
+                $sqlabsen2 = $connection->prepare("SELECT ket FROM absen WHERE ids = ? AND idj = ? AND tgl = ?");
+                $sqlabsen2->bind_param("sis", $siswa['ids'], $idj, $tglnya['tgl']);
+                $sqlabsen2->execute();
+                $absen_notes = $sqlabsen2->get_result();
+                while ($ketnya = $absen_notes->fetch_assoc()) {
+                ?>
+                  <td class="text-center"><?php echo htmlspecialchars($ketnya['ket']); ?></td>
+                <?php } ?>
+              </tr>
+            <?php } ?>
+          </table>
+        </div>
+        <!-- /.table-responsive -->
+        <div class="well">
+          <h4>Keterangan Absensi</h4>
+          <p>A = Tidak Masuk Tanpa Keterangan</p>
+          <p>I = Tidak Masuk Ada Surat Ijin Atau Pemberitahuan</p>
+          <p>S = Tidak Masuk Ada Surat Dokter Atau Pemberitahuan</p>
+          <p>M = Hadir</p>
+        </div>
+      </div>
+      <!-- /.panel-body -->
+    </div>
+    <!-- /.panel -->
+  </div>
+  <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
