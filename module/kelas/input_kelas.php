342e88f41b3c0e8d18bc4325b814486d117a1451
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-29 21:50:50 +0700

    Fix Fitur Kelas

--- module/kelas/input_kelas.php
+++ module/kelas/input_kelas.php
@@ -1,143 +1,133 @@
 <?php
-if($_GET['act']=="input"){
-	?>
-          <div class="row">
-                <div class="col-lg-12">
-					<h3 class="page-header"><strong>Input Data Kelas</strong></h3>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Input Data Kelas
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                    <form method="post" role="form" action="././module/simpan.php?act=input_kelas">
-
-                                <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Nama Sekolah</label>
-                                            <select class="form-control" name="id">
-  <?php
-	$sql=mysql_query("select * from sekolah");
-	while($rs=mysql_fetch_array($sql)){
-if($_SESSION['level']=="admin_guru"){
-if($rs['id']==$_SESSION['id']){
-
-	echo "<option value='$rs[id]'>$rs[nama]</option>";
-}
-}else{
-		echo "<option value='$rs[id]'>$rs[nama]</option>";
-
-	}
-}
+if ($_GET['act'] === "input") {
 ?>
-                                          </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Nama Kelas</label>
-                                            <input class="form-control" placeholder="Kelas" name="nama">
-                                        </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3 class="page-header"><strong>Input Data Kelas</strong></h3>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Input Data Kelas
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <form method="post" role="form" action="./module/simpan.php?act=input_kelas">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label>Nama Sekolah</label>
+                                    <select class="form-control" name="id">
+                                        <?php
+                                        // Use prepared statements to prevent SQL injection
+                                        $stmt = $connection->prepare("SELECT * FROM sekolah");
+                                        $stmt->execute();
+                                        $result = $stmt->get_result();
 
-
-                                        <button type="submit" class="btn btn-success">Simpan</button>
+                                        while ($row = $result->fetch_assoc()) {
+                                            // Display options based on user level
+                                            if ($_SESSION['level'] === "admin_guru") {
+                                                if ($row['id'] === $_SESSION['id']) {
+                                                    echo "<option value='{$row['id']}'>{$row['nama']}</option>";
+                                                }
+                                            } else {
+                                                echo "<option value='{$row['id']}'>{$row['nama']}</option>";
+                                            }
+                                        }
+                                        ?>
+                                    </select>
                                 </div>
-                                <!-- /.col-lg-6 (nested) -->
-                                    </form>
-
+                                <div class="form-group">
+                                    <label>Nama Kelas</label>
+                                    <input class="form-control" placeholder="Kelas" name="nama">
+                                </div>
+                                <button type="submit" class="btn btn-success">Simpan</button>
                             </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
+                            <!-- /.col-lg-6 (nested) -->
+                        </form>
                     </div>
-                    <!-- /.panel -->
+                    <!-- /.row (nested) -->
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.panel-body -->
             </div>
-            <!-- /.row -->
-           <?php } ?>
-
-
-
-           <?php
-if($_GET['act']=="edit_kelas"){
-	?>
-          <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Edit Data Kelas</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Edit Data Kelas
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
 <?php
-                            	$sql=mysql_query("select * from kelas where idk='$_GET[idk]'");
-								$rs=mysql_fetch_array($sql);
+}
 
+if ($_GET['act'] === "edit_kelas") {
 ?>
-                                    <form method="post" role="form" action="././module/simpan.php?act=edit_kelas">
-<input type="hidden" name="idk" value="<?php echo $_GET['idk'] ?>" />
-
-                                <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Kelas</label>
-                                            <select class="form-control" name="id">
-  <?php
-	$sqla=mysql_query("select * from sekolah");
-	while($rsa=mysql_fetch_array($sqla)){
-if($_SESSION['level']=="admin_guru"){
-if($rsa['id']==$_SESSION['id']){
-
-if($rs['id']==$rsa['id']){
-
-	echo "<option value='$rsa[id]' selected='selected'>$rsa[nama]</option>";
-}else{
-	echo "<option value='$rsa[id]'>$rsa[nama]</option>";
-}
-
-}
-}else{
-if($rs['id']==$rsa['id']){
-
-	echo "<option value='$rsa[id]' selected='selected'>$rsa[nama]</option>";
-}else{
-	echo "<option value='$rsa[id]'>$rsa[nama]</option>";
-}
-
-}
-}
-?>
-                                          </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Nama Sekolah</label>
-                                            <input class="form-control" placeholder="Kelas" name="nama" value="<?php echo $rs['nama'] ?>">
-                                        </div>
-
-                                        <button type="submit" class="btn btn-success">Simpan</button>
-                                </div>
-                                <!-- /.col-lg-6 (nested) -->
-                                    </form>
-
-                            </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Edit Data Kelas</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Edit Data Kelas
                 </div>
-                <!-- /.col-lg-12 -->
+                <div class="panel-body">
+                    <div class="row">
+                        <?php
+                        // Use prepared statements to prevent SQL injection
+                        $stmt = $connection->prepare("SELECT * FROM kelas WHERE idk = ?");
+                        $stmt->bind_param("i", $_GET['idk']);
+                        $stmt->execute();
+                        $kelas = $stmt->get_result()->fetch_assoc();
+
+                        ?>
+                        <form method="post" role="form" action="./module/simpan.php?act=edit_kelas">
+                            <input type="hidden" name="idk" value="<?php echo htmlspecialchars($_GET['idk']); ?>" />
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label>Kelas</label>
+                                    <select class="form-control" name="id">
+                                        <?php
+                                        $stmt_sekolah = $connection->prepare("SELECT * FROM sekolah");
+                                        $stmt_sekolah->execute();
+                                        $result_sekolah = $stmt_sekolah->get_result();
+
+                                        while ($row = $result_sekolah->fetch_assoc()) {
+                                            $selected = ($kelas['id'] === $row['id']) ? 'selected="selected"' : '';
+                                            if ($_SESSION['level'] === "admin_guru") {
+                                                if ($row['id'] === $_SESSION['id']) {
+                                                    echo "<option value='{$row['id']}' $selected>{$row['nama']}</option>";
+                                                }
+                                            } else {
+                                                echo "<option value='{$row['id']}' $selected>{$row['nama']}</option>";
+                                            }
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Nama Sekolah</label>
+                                    <input class="form-control" placeholder="Kelas" name="nama" value="<?php echo htmlspecialchars($kelas['nama']); ?>">
+                                </div>
+                                <button type="submit" class="btn btn-success">Simpan</button>
+                            </div>
+                            <!-- /.col-lg-6 (nested) -->
+                        </form>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
             </div>
-            <!-- /.row -->
-            <?php } ?>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+<?php
+}
+?>
--- module/kelas/kelas.php
+++ module/kelas/kelas.php
@@ -1,73 +1,81 @@
-            <div class="row">
-                <div class="col-lg-12">
-					<h3 class="page-header"><strong>Data Kelas</strong></h3>
-                </div>
-                <!-- /.col-lg-12 -->
+<div class="row">
+    <div class="col-lg-12">
+        <h3 class="page-header"><strong>Data Kelas</strong></h3>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                Data Kelas
             </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Data Kelas
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">Kode Sekolah</th>
-                                            <th class="text-center">Nama Sekolah</th>
-                                            <th class="text-center">Kelas</th>
-                                            <th class="text-center">Aksi</th>
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Kode Sekolah</th>
+                                <th class="text-center">Nama Sekolah</th>
+                                <th class="text-center">Kelas</th>
+                                <th class="text-center">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            // Use prepared statements to prevent SQL injection
+                            $stmt = $connection->prepare("SELECT * FROM kelas");
+                            $stmt->execute();
+                            $result = $stmt->get_result();
 
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-<?php
-$no=1;
-	$sql=mysql_query("select * from kelas");
-	while($rs=mysql_fetch_array($sql)){
-	$sqla=mysql_query("select * from sekolah where id='$rs[id]'");
-	$rsa=mysql_fetch_array($sqla);
-if($_SESSION['level']=="admin_guru"){
-if($rsa['id']==$_SESSION['id']){
+                            while ($rs = $result->fetch_assoc()) {
+                                // Fetch related school data
+                                $stmt_school = $connection->prepare("SELECT * FROM sekolah WHERE id = ?");
+                                $stmt_school->bind_param("i", $rs['id']);
+                                $stmt_school->execute();
+                                $rsa = $stmt_school->get_result()->fetch_assoc();
 
-?>                                        <tr class="odd gradeX">
-                                            <td><?php echo"$rsa[kode]";  ?></td>
-                                            <td><?php echo"$rsa[nama]";  ?></td>
-                                            <td class="text-center"><?php echo"$rs[nama]";  ?></td>
-
-                                        <td class="text-center"><a href="./././media.php?module=input_kelas&act=edit_kelas&idk=<?php echo $rs['idk'] ?>"><button type="button" class="btn btn-info">Edit</button> <a href="././module/simpan.php?act=hapus_kelas&idk=<?php echo $rs['idk'] ?>"><button type="button" class="btn btn-danger">Hapus</button></a></td>
-
-                                        </tr>
-<?php
-}
-}else{
-?>
-                                        <tr class="odd gradeX">
-                                            <td><?php echo"$rsa[kode]";  ?></td>
-                                            <td><?php echo"$rsa[nama]";  ?></td>
-                                            <td class="text-center"><?php echo"$rs[nama]";  ?></td>
-
-                                        <td class="text-center"><a href="./././media.php?module=input_kelas&act=edit_kelas&idk=<?php echo $rs['idk'] ?>"><button type="button" class="btn btn-info">Edit</button> <a href="././module/simpan.php?act=hapus_kelas&idk=<?php echo $rs['idk'] ?>"><button type="button" class="btn btn-danger">Hapus</button></a></td>
-
-                                        </tr>
-
-<?php
-	}
-}
-?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
+                                // Check user level and display actions accordingly
+                                $is_admin_guru = $_SESSION['level'] === "admin_guru";
+                                if ($is_admin_guru && $rsa['id'] === $_SESSION['id']) {
+                            ?>
+                                    <tr class="odd gradeX">
+                                        <td><?php echo htmlspecialchars($rsa['kode']); ?></td>
+                                        <td><?php echo htmlspecialchars($rsa['nama']); ?></td>
+                                        <td class="text-center"><?php echo htmlspecialchars($rs['nama']); ?></td>
+                                        <td class="text-center">
+                                            <a href="./media.php?module=input_kelas&act=edit_kelas&idk=<?php echo urlencode($rs['idk']); ?>" class="btn btn-info">Edit</a>
+                                            <a href="./module/simpan.php?act=hapus_kelas&idk=<?php echo urlencode($rs['idk']); ?>" class="btn btn-danger">Hapus</a>
+                                        </td>
+                                    </tr>
+                                <?php
+                                } else {
+                                ?>
+                                    <tr class="odd gradeX">
+                                        <td><?php echo htmlspecialchars($rsa['kode']); ?></td>
+                                        <td><?php echo htmlspecialchars($rsa['nama']); ?></td>
+                                        <td class="text-center"><?php echo htmlspecialchars($rs['nama']); ?></td>
+                                        <td class="text-center">
+                                            <a href="./media.php?module=input_kelas&act=edit_kelas&idk=<?php echo urlencode($rs['idk']); ?>" class="btn btn-info">Edit</a>
+                                            <a href="./module/simpan.php?act=hapus_kelas&idk=<?php echo urlencode($rs['idk']); ?>" class="btn btn-danger">Hapus</a>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.table-responsive -->
             </div>
-            <!-- /.row -->
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
