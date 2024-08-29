c2b26a145fba2e743722a5d90e9bcdc78ba8bb5b
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-29 21:03:01 +0700

    fix Pages Utama

--- ceklog.php
+++ ceklog.php
@@ -1,64 +1,69 @@
 <?php
 // FILE KONEKSI
+include "config/conn.php";
 
-include"config/conn.php";
+// Ambil data dari form
+$pass = md5($_POST['password']);
+$passw = $_POST['password'];
+$user = $_POST['username'];
 
-$pass=md5($_POST['password']);
-$passw=$_POST['password'];
+// Sanitize input untuk mencegah SQL Injection
+$user = mysqli_real_escape_string($connection, $user);
 
-$user=$_POST['username'];
+// Query untuk tabel user
+$sql = mysqli_query($connection, "SELECT * FROM user WHERE nama='$user' AND pass='$pass'");
+$count = mysqli_num_rows($sql);
+$rs = mysqli_fetch_array($sql);
 
-	$sql=mysql_query("select * from user where nama='$user' and pass='$pass'");
-	$count=mysql_num_rows($sql);
-	$rs=mysql_fetch_array($sql);
-		if($count>0){
-			session_start();
-				$_SESSION['idu']=$rs['idu'];
+if ($count > 0) {
+    session_start();
+    $_SESSION['idu'] = $rs['idu'];
+    $_SESSION['nama'] = $rs['nama'];
+    $_SESSION['level'] = $rs['level'];
+    $_SESSION['idk'] = "";
+    $_SESSION['ortu'] = "";
+    $_SESSION['id'] = $rs['id'];
 
-				$_SESSION['nama']=$rs['nama'];
-				$_SESSION['level']=$rs['level'];
-				$_SESSION['idk']="";
-				$_SESSION['ortu']="";
-				$_SESSION['id']=$rs['id'];
+    header('location:media.php?module=home');
+} else {
+    $mr = md5($_POST['password']);
 
+    // Query untuk tabel siswa
+    $sqla = mysqli_query($connection, "SELECT * FROM siswa WHERE nis='$user' AND pass='$mr'");
+    $counta = mysqli_num_rows($sqla);
+    $rsa = mysqli_fetch_array($sqla);
 
-			header('location:media.php?module=home');
-		}else{
-$mr=md5($_POST['password']);
-	$sqla=mysql_query("select * from siswa where nis='$user' and pass='$mr'");
-	$counta=mysql_num_rows($sqla);
-	$rsa=mysql_fetch_array($sqla);
-if($counta>0){
-			session_start();
-				$_SESSION['idu']=$rsa['nis'];
-				$_SESSION['nama']=$rsa['nama'];
-				$_SESSION['level']="user";
-				$_SESSION['ortu']=$passw;
-				$_SESSION['idk']=$rsa['idk'];
-				$_SESSION['id']="2";
+    if ($counta > 0) {
+        session_start();
+        $_SESSION['idu'] = $rsa['nis'];
+        $_SESSION['nama'] = $rsa['nama'];
+        $_SESSION['level'] = "user";
+        $_SESSION['ortu'] = $passw;
+        $_SESSION['idk'] = $rsa['idk'];
+        $_SESSION['id'] = "2";
 
-			header('location:media.php?module=home');
+        header('location:media.php?module=home');
+    } else {
+        $gr = md5($_POST['password']);
 
-}else{
-$gr=md5($_POST['password']);
-	$sqlz=mysql_query("select * from guru where nip='$user' and pass='$gr'");
-	$countz=mysql_num_rows($sqlz);
-	$rsz=mysql_fetch_array($sqlz);
-if($countz>0){
-			session_start();
-				$_SESSION['idu']=$rsz['nip'];
-				$_SESSION['nama']=$rsz['nama'];
-				$_SESSION['idk']=$rsz['idk'];
-				$_SESSION['level']="guru";
-				$_SESSION['ortu']="";
-				$_SESSION['id']="2";
+        // Query untuk tabel guru
+        $sqlz = mysqli_query($connection, "SELECT * FROM guru WHERE nip='$user' AND pass='$gr'");
+        $countz = mysqli_num_rows($sqlz);
+        $rsz = mysqli_fetch_array($sqlz);
 
-			header('location:media.php?module=home');
+        if ($countz > 0) {
+            session_start();
+            $_SESSION['idu'] = $rsz['nip'];
+            $_SESSION['nama'] = $rsz['nama'];
+            $_SESSION['idk'] = $rsz['idk'];
+            $_SESSION['level'] = "guru";
+            $_SESSION['ortu'] = "";
+            $_SESSION['id'] = "2";
 
-
-}else{
-			echo "<script>alert('Mohon periksa kembali Username & Password Anda'); location.href='login.php';</script>";
-}
-}
+            header('location:media.php?module=home');
+        } else {
+            echo "<script>alert('Mohon periksa kembali Username & Password Anda'); location.href='login.php';</script>";
+        }
+    }
 }
 ?>
