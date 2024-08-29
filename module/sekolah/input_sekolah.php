7d7cea4a413fbc0155509ef9806ba2e11884c87f
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-29 21:32:52 +0700

    Fix Fitur Sekolah

--- module/sekolah/input_sekolah.php
+++ module/sekolah/input_sekolah.php
@@ -1,59 +1,65 @@
-         <?php
-if($_GET['act']=="edit_sekolah"){
-	?>
-          <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Edit Data Sekolah</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            Edit Data Sekolah
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
 <?php
-                            	$sql=mysql_query("select * from sekolah where id='$_GET[id]'");
-								$rs=mysql_fetch_array($sql);
+if ($_GET['act'] == "edit_sekolah") {
+    // Sanitize the id parameter
+    $id = $connection->real_escape_string($_GET['id']);
 
+    // Query to fetch the school data
+    $sql = "SELECT * FROM sekolah WHERE id='$id'";
+    $result = $connection->query($sql);
+
+    if ($result->num_rows > 0) {
+        $rs = $result->fetch_assoc();
 ?>
-                                    <form method="post" role="form" action="././module/simpan.php?act=edit_sekolah">
-<input type="hidden" name="id" value="<?php echo $_GET['id'] ?>" />
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Edit Data Sekolah</h1>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        Edit Data Sekolah
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <form method="post" role="form" action="././module/simpan.php?act=edit_sekolah">
+                                <input type="hidden" name="id" value="<?php echo htmlspecialchars($id, ENT_QUOTES, 'UTF-8'); ?>" />
 
                                 <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Kode Sekolah</label>
-                                            <input class="form-control" placeholder="Kode" name="kode" value="<?php echo "$rs[kode]"; ?>">
-</div>
-                                        <div class="form-group">
-
-                                            <label>Nama Sekolah</label>
-                                            <input class="form-control" placeholder="Nama Sekolah" name="nama" value="<?php echo "$rs[nama]"; ?>">
-
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label>Alamat</label>
-                                            <textarea class="form-control" placeholder="Alamat" name="alamat" rows="3"><?php echo "$rs[alamat]"; ?></textarea>
-                                        </div>
-
-                                        <button type="submit" class="btn btn-success">Simpan</button>
+                                    <div class="form-group">
+                                        <label>Kode Sekolah</label>
+                                        <input class="form-control" placeholder="Kode" name="kode" value="<?php echo htmlspecialchars($rs['kode'], ENT_QUOTES, 'UTF-8'); ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Nama Sekolah</label>
+                                        <input class="form-control" placeholder="Nama Sekolah" name="nama" value="<?php echo htmlspecialchars($rs['nama'], ENT_QUOTES, 'UTF-8'); ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Alamat</label>
+                                        <textarea class="form-control" placeholder="Alamat" name="alamat" rows="3"><?php echo htmlspecialchars($rs['alamat'], ENT_QUOTES, 'UTF-8'); ?></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-success">Simpan</button>
                                 </div>
                                 <!-- /.col-lg-6 (nested) -->
-                                    </form>
-
-                            </div>
-                            <!-- /.row (nested) -->
+                            </form>
                         </div>
-                        <!-- /.panel-body -->
+                        <!-- /.row (nested) -->
                     </div>
-                    <!-- /.panel -->
+                    <!-- /.panel-body -->
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.panel -->
             </div>
-            <!-- /.row -->
-            <?php } ?>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+<?php
+    } else {
+        echo "No records found.";
+    }
+
+    $connection->close();
+}
+?>
--- module/sekolah/sekolah.php
+++ module/sekolah/sekolah.php
@@ -1,53 +1,62 @@
-            <div class="row">
-                <div class="col-lg-12">
-					<h3 class="page-header"><strong>Data Sekolah</strong></h3>
+<div class="row">
+    <div class="col-lg-12">
+        <h3 class="page-header"><strong>Data Sekolah</strong></h3>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-primary">
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Kode</th>
+                                <th class="text-center">Nama Sekolah</th>
+                                <th class="text-center">Alamat</th>
+                                <th class="text-center">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "SELECT * FROM sekolah";
+                            $result = $connection->query($sql);
+
+                            if ($result->num_rows > 0) {
+                                while ($rs = $result->fetch_assoc()) {
+                            ?>
+                                    <tr class="odd gradeX">
+                                        <td><?php echo htmlspecialchars($rs['kode'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                        <td><?php echo htmlspecialchars($rs['nama'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                        <td><?php echo htmlspecialchars($rs['alamat'], ENT_QUOTES, 'UTF-8'); ?></td>
+                                        <td class="text-center">
+                                            <a href="./././media.php?module=input_sekolah&act=edit_sekolah&id=<?php echo urlencode($rs['id']); ?>">
+                                                <button type="button" class="btn btn-info">Edit</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                            } else {
+                                echo "<tr><td colspan='4' class='text-center'>No records found</td></tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.table-responsive -->
             </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
 
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">Kode</th>
-                                            <th class="text-center">Nama Sekolah</th>
-                                            <th class="text-center">Alamat</th>
-                                            <th class="text-center">Aksi</th>
-
-                                        </tr>
-                                    </thead>
-                                    <tbody>
 <?php
-$no=1;
-	$sql=mysql_query("select * from sekolah");
-	while($rs=mysql_fetch_array($sql)){
-
-?>                                        <tr class="odd gradeX">
-                                            <td><?php echo"$rs[kode]";  ?></td>
-                                            <td ><?php echo"$rs[nama]";  ?></td>
-                                            <td ><?php echo"$rs[alamat]";  ?></td>
-
-                                        <td class="text-center"><a href="./././media.php?module=input_sekolah&act=edit_sekolah&id=<?php echo $rs['id'] ?>"><button type="button" class="btn btn-info">Edit</button></a></td>
-
-                                        </tr>
-<?php
-}
+$connection->close();
 ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
